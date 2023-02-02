c32f6b7d49a91380aeba728555117044351f9f0a
Mustafa Saifee <29223820+mustafasaifee42@users.noreply.github.com>
2023-02-02 19:24:54 +0200

    Update Header.tsx

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -7,7 +7,7 @@ export function Header() {
     <>
       <div className='flex-div flex-space-between flex-vert-align-center' style={{ padding: 'var(--spacing-05)', backgroundColor: 'var(--gray-200)' }}>
         <h4 className='undp-typography margin-bottom-00 bold'>
-          Energy Access from Space
+          Electricity Access from Space
         </h4>
         <button className='undp-button button-primary' type='button' onClick={() => { setIsModalVisible(true); }}>
           Methodology
